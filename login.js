213f083f3caec478fbff856082edafabd3573147
Arihant Jain <jainarihant195@gmail.com>
2023-03-24 22:39:19 +0530

    login.js

--- login.js
+++ login.js
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'static')));
 // http://localhost:3000/
 app.get('/', function(request, response) {
 	// Render login template
-	response.sendFile(path.join(__dirname + '/login.html'));
+	response.sendFile(path.join(__dirname + '/demo.html'));
 });
 
 // http://localhost:3000/auth
